6947327ffd3734d8a09ede4bb4696321436a17f4
Aniket <aniket.d2402@outlook.com>
2020-09-20 19:50:45 -0400

    Service files added

--- CollegeGrpcService/Services/CourseService.cs
+++ CollegeGrpcService/Services/CourseService.cs
@@ -2,17 +2,70 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CollegeAPI.Controllers;
+using CollegeGrpcService.Protos;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 using static CollegeGrpcService.Protos.Course;
+using Course = CollegeAPI.Models.Course;
 
 namespace CollegeGrpcService
 {
     public class CourseService: CourseBase
     {
+        private readonly CourseController logic;
+
         public CourseService()
         {
+            logic = new CourseController();
+        }
 
+        public override Task<CourseReply> GetCourse(CourseIdRequest request, ServerCallContext context)
+        {
+            Course course = logic.GetCourse(request.CourseID);
+            return Task.FromResult<CourseReply>(FromPoco(course));
+        }
+
+        public override Task<Courses> GetAllCourses(Empty request, ServerCallContext context)
+        {
+            Courses courses = new Courses();
+            List<Course> allCourses = logic.GetAllCourses();
+            foreach (var course in allCourses)
+            {
+                courses.Reply.Add(FromPoco(course));
+            }
+            return Task.FromResult<Courses>(courses);
+        }
+
+        public override Task<Empty> AddCourse(CourseReply request, ServerCallContext context)
+        {
+            Course course = new Course
+            {
+                CourseID = request.CourseID,
+                CourseName = request.CourseName
+            };
+            logic.AddCourse(course);
+            return Task.FromResult(new Empty());
+
+        }
+
+        private CourseReply FromPoco(Course poco)
+        {
+            return new CourseReply
+            {
+                CourseID = poco.CourseID,
+                CourseName = poco.CourseName
+            };
+        }
+
+        private Course ToPoco(CourseReply reply)
+        {
+            return new Course
+            {
+                CourseID = reply.CourseID,
+                CourseName = reply.CourseName
+            };
         }
     }
 }
--- CollegeGrpcService/Services/StudentService.cs
+++ CollegeGrpcService/Services/StudentService.cs
@@ -1,11 +1,69 @@
-﻿using System;
+﻿using CollegeAPI.Controllers;
+using CollegeGrpcService.Protos;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static CollegeGrpcService.Protos.Student;
+using Student = CollegeAPI.Models.Student;
 
 namespace CollegeGrpcService.Services
 {
-    public class StudentService
+    public class StudentService: StudentBase
     {
+        private readonly StudentController logic;
+
+        public StudentService()
+        {
+            logic = new StudentController();
+        }
+
+        public override Task<StudentReply> GetStudent(StudentIdRequest request, ServerCallContext context)
+        {
+            Student student = logic.GetStudent(request.StudentID);
+            return Task.FromResult<StudentReply>(FromPoco(student));
+        }
+
+        public override Task<Students> GetAllStudents(Empty request, ServerCallContext context)
+        {
+            Students students = new Students();
+            List<Student> allStudents = logic.GetAllStudents();
+            foreach (var student in allStudents)
+            {
+                students.Reply.Add(FromPoco(student));
+            }
+            return Task.FromResult<Students>(students);
+        }
+
+        public override Task<Empty> AddStudent(StudentReply request, ServerCallContext context)
+        {
+            Student student = new Student
+            {
+                StudentID = request.StudentID,
+                StudentName = request.Name
+            };
+            logic.AddStudent(student);
+            return Task.FromResult(new Empty());
+        }
+
+        private StudentReply FromPoco(Student poco)
+        {
+            return new StudentReply
+            {
+                StudentID = poco.StudentID,
+                Name = poco.StudentName
+            };
+        }
+
+        private Student ToPoco(StudentReply reply)
+        {
+            return new Student
+            {
+                StudentID = reply.StudentID,
+                StudentName = reply.Name
+            };
+        }
     }
 }
