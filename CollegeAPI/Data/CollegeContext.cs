d4777eeeee7323ca4db6a73120dd1bc5304f1817
Aniket <aniket.d2402@outlook.com>
2020-09-20 19:50:11 -0400

    CollegeContext added

---
+++ CollegeAPI/Data/CollegeContext.cs
@@ -0,0 +1,26 @@
+﻿using CollegeAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeAPI.Data
+{
+    public class CollegeContext:DbContext
+    {
+        public CollegeContext(DbContextOptions<CollegeContext> options):base(options){}
+
+        public CollegeContext(){}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("CollegeDB");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+    }
+}
